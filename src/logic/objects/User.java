3fa41ca72f8d19e27403a7e752660f2371ca00da
2dam <you@example.com>
2022-10-26 12:37:50 +0200

    class User has been modified

--- src/logic/objects/User.java
+++ src/logic/objects/User.java
@@ -106,8 +106,11 @@ public class User implements Comparable <User> {
         lastLogins = pLastLogins;
     }
     private String lastLoginString() {
-        // TODO: generate string.
-        return null;
+        String ls = "";
+        lastLogins
+            .stream()
+            .forEach(t -> ls.concat("\n" + t.toString()));
+        return ls;
     }
 
     @Override
