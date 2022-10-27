b5e8d93f5e5317b6f6834df9f71a94dfb2bd6bdc
RokeIturralde <rokeitumendi@gmail.com>
2022-10-27 12:22:48 +0200

    started the process of connecting to the client

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,26 @@
 package app;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import logic.objects.User;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ServerSocket miServidor = null;
+        Socket socket = null;
+        ObjectOutputStream salida = null;
+        ObjectInputStream entrada = null;
+        User user = new User();
+
+        miServidor = new ServerSocket(7777);
+        socket = miServidor.accept();
+
+        //Lectura de Instruccion en el lado Cliente
+        entrada = new ObjectInputStream(socket.getInputStream());
+        user = (User) entrada.readObject();
+        user.toString();
     }
 }
