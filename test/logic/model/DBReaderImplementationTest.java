4d93652f3973f80966ee109511f3c8a076a4ac22
optickx <83361124+optickx@users.noreply.github.com>
2022-11-02 09:41:31 +0100

    Add files via upload

--- test/logic/model/DBReaderImplementationTest.java
+++ test/logic/model/DBReaderImplementationTest.java
@@ -7,10 +7,7 @@ package logic.model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.ResourceBundle;
-import logic.DBAFactory;
+
 import logic.objects.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
@@ -25,36 +22,33 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class DBReaderImplementationTest {
     
-    @BeforeAll
-    public static void setUpClass() {
-        Connection con = null;
-        ResourceBundle rb = 
-            ResourceBundle
-                .getBundle("resources.database_access");
-
-        try {
-            con = DriverManager
-                .getConnection(
-                    rb.getString("URL"),
-                    rb.getString("USER"),
-                    rb.getString("PASSWORD"));
-        } catch (SQLException sqle) {
-            // TODO: handle exception
-        }
-
-        DBAFactory.provisionalSystemStartConnection(con);
-    }
+    public static DBReaderImplementation dbi;
     
-    @AfterAll
-    public static void tearDownClass() {
+    @BeforeEach
+    public static void setUpClass() {
+        Connection c = null;
+        try {
+            c = DriverManager
+                .getConnection(
+                "url",
+                "root",
+                "abcd*1234");
+        } catch (SQLException sqle) {
+
+        }
+        
+        dbi =
+            new DBReaderImplementation(c);
     }
     
     @BeforeEach
     public void setUp() {
+
     }
     
     @AfterEach
     public void tearDown() {
+        
     }
 
     /**
@@ -62,14 +56,8 @@ public class DBReaderImplementationTest {
      */
     @Test
     public void testSignIn() {
-        System.out.println("signIn");
-        User pUser = null;
-        DBReaderImplementation instance = null;
-        User expResult = null;
-        User result = instance.signIn(pUser);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        
     }
 
     /**
@@ -77,18 +65,9 @@ public class DBReaderImplementationTest {
      */
     @Test
     public void testSignUp() {
-        User u2, u1 =
-            new User(
-            0, "opticks", "danielbarrios2002@gmail.com", 
-            "Daniel Barrios", "abcd*1234",
-            Timestamp.valueOf(LocalDateTime.now()), 1, 1, null);
+        
+        
 
-        System.out.println(u1.toString());
-
-        u2 = DBAFactory.getAccess().signUp(null);
-
-        assertEquals(u1, u2);
-        System.out.println(u2);
     }
 
     /**
@@ -96,13 +75,7 @@ public class DBReaderImplementationTest {
      */
     @Test
     public void testGenerateID() {
-        System.out.println("generateID");
-        DBReaderImplementation instance = null;
-        int expResult = 0;
-        int result = instance.generateID();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
@@ -110,13 +83,8 @@ public class DBReaderImplementationTest {
      */
     @Test
     public void testGetConnection() {
-        System.out.println("getConnection");
-        DBReaderImplementation instance = null;
-        Connection expResult = null;
-        Connection result = instance.getConnection();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        
     }
     
 }
