c4db37cc9953b83e52b8e6fdb1f67bbd738edd11
RokeIturralde <rokeitumendi@gmail.com>
2022-11-10 13:40:32 +0100

    nothing changed

--- src/app/App.java
+++ src/app/App.java
@@ -1,11 +1,6 @@
 package app;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 import logic.ControllerThread;
-import logic.objects.message.Request;
 
 public class App {
 
