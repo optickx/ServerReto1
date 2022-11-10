db7b46a3dc24f7e31a6753e6e525a3949e83bec6
optickx <83361124+optickx@users.noreply.github.com>
2022-11-10 09:48:23 +0100

    Update App.java

--- src/app/App.java
+++ src/app/App.java
@@ -1,18 +1,10 @@
 package app;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 import logic.ControllerThread;
-import logic.objects.message.Request;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-
-        ControllerThread controller = new ControllerThread();
-        controller.run();
-
+        new ControllerThread().run();
     }
 }
